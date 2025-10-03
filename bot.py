6406db6990aed8f39b4a5a793f6385c53ec06162
chandircity123456-gif <chandircity123456@gmail.com>
2025-10-03 10:21:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,12 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters, Com
 from ismlar import ISMLAR_LUGATI 
 
 # !!! BOT TOKENINGIZNI BU YERGA QO'YING. RENDER BU QATORNI O'ZI UCHUN ISHLATADI !!!
-# Renderdagi xatolar tufayli tokenni to'g'ridan-to'g'ri kiritdik.
 TOKEN = "8422115593:AAH_9RJtYUSp8IyDfdt9qbKsDoaC0tSjuZE" 
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL", None)
 
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Apostrof xatolari to'g'irlandi
+    # Barcha satrlar qo'sh tirnoqda, ichki yakka tirnoq esa bexato ishlatilgan
     xabar = "Assalomu alaykum! Ismlar lug'ati botiga xush kelibsiz.\n"
     xabar += "Menga ma'nosini bilmoqchi bo'lgan **ismni yuboring**."
     await update.message.reply_text(xabar)
@@ -21,10 +20,10 @@ async def ism_qidirish(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     
     if kalit_soz in ISMLAR_LUGATI:
         ma'nosi = ISMLAR_LUGATI[kalit_soz]
-        # Xato to'g'irlandi (ma'nosi so'zi ishtirok etgan joy)
+        # Xato to'g'irlandi
         javob = f"**{qidiriladigan_ism.capitalize()}** ismining ma'nosi:\n\n_{ma'nosi}_" 
     else:
-        # Xato to'g'irlandi (Kechirasiz so'zi ishtirok etgan joy)
+        # Xato to'g'irlandi
         javob = f"Kechirasiz, **{qidiriladigan_ism.capitalize()}** ismining ma'nosi lug'atda topilmadi."
 
     await update.message.reply_text(javob, parse_mode='Markdown')
